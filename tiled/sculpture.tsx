e3e5a6e4dc1aae9aa71a8d53f3c9cf5a8b03eb31
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-01-10 17:29:18 -0500

    added sculpture

---
+++ tiled/sculpture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="sculpture" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="sculpture.png" width="128" height="128"/>
+</tileset>
